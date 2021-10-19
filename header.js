07cb1fa027bccf679327259d32f97a20a4c39211
chienchien2000 <andforyou2000@gmail.com>
2021-10-19 22:24:08 +0700

    add header

---
+++ header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function header() {
+    return (
+        <View>
+            <Text>dgffd</Text>
+        </View>
+    )
+}
